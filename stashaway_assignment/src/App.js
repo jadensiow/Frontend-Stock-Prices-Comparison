a2c7acabd6c9c62642b3b71ea018a2fc03e12d42
jadensiow <jianhong93@gmail.com>
2021-06-10 01:03:38 +0800

    added api call

--- stashaway_assignment/src/App.js
+++ stashaway_assignment/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  console.log(process.env.REACT_APP_YAHOO_API_KEY);
+  const config = {
+    headers: {
+      "x-rapidapi-key": process.env.REACT_APP_YAHOO_API_KEY,
+      "x-rapidapi-host": process.env.REACT_APP_YAHOO_API_HOST,
+      useQueryString: true,
+    },
+    params: {
+      symbol: "VTSMX",
+      region: "US",
+    },
+  };
+
+  axios
+    .get(
+      `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-historical-data`,
+      config
+    )
+    .then((resp) => console.log(resp))
+    .catch((err) => console.log(err));
+
+  return <div className="App"></div>;
+};
 
 export default App;
